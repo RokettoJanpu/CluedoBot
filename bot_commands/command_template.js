153304e8e64d0798b17b3f4093f6d08ea557a22c
RokettoJanpu <rokettojanpu@gmail.com>
2019-04-01 19:14:16 -0400

    Update and rename command_template.js to command_template.txt

--- bot_commands/command_template.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: `Enter the command name in lowercase without the prefix.`,
-	description: `Enter your command description here. Feel free to be funny if you like.`,
-	execute(message, user, atUser, toUser, query, queryString, args, mentions){
-        botResponse = `Pong`;
-		message.channel.send(botResponse);
-		logResponse = botResponse;
-	},
-};
