604bf24c4fdda74a97b826b24737560d49df5b13
RokettoJanpu <rokettojanpu@gmail.com>
2019-04-01 03:44:26 -0400

    e

---
+++ bot_commands/murder.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: `ping`,
+	description: `A murder, you say...?`,
+	execute(message, user, atUser, toUser, query, queryString, args, mentions){
+        botResponse = `:skull_crossbones: A murder, you say? I do declare! :dagger:`;
+		message.channel.send(botResponse);
+		logResponse = botResponse;
+	},
+};
