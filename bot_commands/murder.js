595fb10da2d4f616b6c19a8731e1235fde56f84a
RokettoJanpu <rokettojanpu@gmail.com>
2019-04-01 18:58:58 -0400

    Update murder.js

--- bot_commands/murder.js
+++ bot_commands/murder.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: `murder`,
 	description: `A murder, you say...?`,
 	execute(message, user, atUser, toUser, query, queryString, args, mentions){
-        botResponse = `:skull_crossbones: A murder, you say? I do declare! :dagger:`;
+        	botResponse = `:skull_crossbones: A murder, you say? I do declare! :dagger:`;
 		message.channel.send(botResponse);
 		logResponse = botResponse;
 	},
