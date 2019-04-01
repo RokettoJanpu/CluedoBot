885d9def86a034fc0ee74cab346d3da05f4421c5
RokettoJanpu <rokettojanpu@gmail.com>
2019-04-01 03:51:01 -0400

    Update murder.js

--- bot_commands/murder.js
+++ bot_commands/murder.js
@@ -1,5 +1,5 @@
 module.exports = {
-	name: `ping`,
+	name: `murder`,
 	description: `A murder, you say...?`,
 	execute(message, user, atUser, toUser, query, queryString, args, mentions){
         botResponse = `:skull_crossbones: A murder, you say? I do declare! :dagger:`;
