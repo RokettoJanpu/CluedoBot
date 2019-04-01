e95f6e2c39d3f9ca0ab809c580b3b40f7ca0f176
RokettoJanpu <rokettojanpu@gmail.com>
2019-04-01 18:58:47 -0400

    Update ping.js

--- bot_commands/ping.js
+++ bot_commands/ping.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: `ping`,
 	description: `Pong!`,
 	execute(message, user, atUser, toUser, query, queryString, args, mentions){
-        botResponse = `Pong! :ping_pong:`;
+        	botResponse = `Pong! :ping_pong:`;
 		message.channel.send(botResponse);
 		logResponse = botResponse;
 	},
